83f510d15837a7172f0043d4984f72e083c586a6
Luiz Carlos <luicfrr+github@gmail.com>
2025-07-21 17:28:29 -0300

    add test app

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react'
+import { MaskedInput } from '@/index'
+
+function Index(): ReactNode {
+
+  return (
+    <MaskedInput
+      mask='000.000.000-00'
+      maskOptions={ {
+        lazy: true
+      } }
+      placeholder='type here'
+    />
+  )
+}
+
+export default Index
